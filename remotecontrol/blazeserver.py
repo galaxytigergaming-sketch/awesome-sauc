71a0f93101bc242bd6c9928fb6d6268a83c93e7c
galaxytigergaming-sketch <galaxytigergaming@gmail.com>
2026-07-07 22:42:35 -0500

    Add files via upload

---
+++ remotecontrol/blazeserver.py
@@ -0,0 +1,38 @@
+﻿from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+latest_command = "none"
+
+
+@app.route("/send", methods=["POST"])
+def send():
+    global latest_command
+
+    data = request.json
+    latest_command = data["command"]
+
+    print("Command:", latest_command)
+
+    return jsonify({
+        "status": "ok"
+    })
+
+
+@app.route("/get", methods=["GET"])
+def get():
+    return jsonify({
+        "command": latest_command
+    })
+
+
+@app.route("/")
+def home():
+    return "Quantum Remote Server Online"
+
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5000
+    )
